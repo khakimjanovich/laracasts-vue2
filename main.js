eefff2fe01e1a07c4311fba058adb8b912b70fa5
Yunusali <37039084+Khakimjanovich@users.noreply.github.com>
2022-05-02 16:03:09 +0500

    E-10: Practical component exercise #2

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-Vue.component('message', {
+Vue.component('modal', {
     data(){
         return{
-            isVisible: true,
+            isVisible: false,
         }
     },
-    props: ['title', 'body'],
-    template: `
-            <article class="message" v-show="isVisible">
-                <div class="message-header">
-                    <p>{{title}}</p>
-                    <button class="delete" aria-label="delete" @click="hideModal"></button>
+    template:
+        `
+            <div class="modal is-active">
+                <div class="modal-background"></div>
+                <div class="modal-content">
+                <div class="box">
+                    <slot></slot>
                 </div>
-                <div class="message-body">
-                    {{body}}
                 </div>
-            </article>`,
-    methods: {
-        hideModal() {
-            this.isVisible = !this.isVisible;
-        }
-    }
+                <button aria-label="close" class="modal-close is-large" @click="$emit('close')"></button>
+            </div>
+        `,
 })
 
 new Vue({
     el: '#root',
+    data:{
+        showModal:false,
+    }
 })
