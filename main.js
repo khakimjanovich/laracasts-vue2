27a720fb5df20bf1ed902abc42bc318ea745ba4e
Yunusali <37039084+Khakimjanovich@users.noreply.github.com>
2022-05-02 19:43:22 +0500

    E-11: Practical component exercise #3

--- main.js
+++ main.js
@@ -1,26 +1,62 @@
-Vue.component('modal', {
-    data(){
-        return{
-            isVisible: false,
-        }
-    },
-    template:
-        `
-            <div class="modal is-active">
-                <div class="modal-background"></div>
-                <div class="modal-content">
-                <div class="box">
-                    <slot></slot>
+Vue.component('tabs', {
+    template: `
+            <div>
+                <div class="tabs">
+                    <ul>
+                        <li v-for="tab in tabs" :class="{ 'is-active' : tab.isActive }"><a :href="tab.href" @click="selectTab(tab)">{{tab.name}}</a></li>
+                    </ul>
                 </div>
+                <div class="tab-details">
+                        <slot></slot>
                 </div>
-                <button aria-label="close" class="modal-close is-large" @click="$emit('close')"></button>
             </div>
         `,
+    data() {
+        return {
+            tabs: [],
+        }
+    },
+    created() {
+        this.tabs = this.$children
+    },
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name === selectedTab.name);
+            })
+        }
+    }
 })
+
+Vue.component('tab', {
+    template:
+            `
+                <div v-if="isActive"><slot></slot></div>  
+            `,
+    props: {
+        name: {
+            required: true
+        },
+        selected: {
+            default: false
+        }
+    },
+    data() {
+        return {
+            isActive: false
+        };
+    },
+    mounted() {
+        this.isActive = this.selected
+    },
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-')
+        }
+    }
+})
+
 
 new Vue({
     el: '#root',
-    data:{
-        showModal:false,
-    }
 })
