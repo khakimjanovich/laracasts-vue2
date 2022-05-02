b90fd5e3513f8332b892dacfd54e1ea45eaa0741
Yunusali <37039084+Khakimjanovich@users.noreply.github.com>
2022-05-02 15:38:30 +0500

    E-08: Components within components

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
+Vue.component('task-list', {
+    template: `
+            <div>
+                <task v-for="task in tasks">
+                    {{ task.description }} {{ task.completed }}
+                </task>
+            </div>`,
+    data() {
+        return {
+            tasks: [{description: 'Go to the store', completed: false}, {
+                description: 'Go to the bank', completed: false
+            }, {description: 'Go to the home', completed: false},]
+        }
+    }
+})
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 })
 
 new Vue({
-    el:'#root',
+    el: '#root',
 })
