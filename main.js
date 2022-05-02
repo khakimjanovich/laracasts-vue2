3f1864f1cc65a117edd8391c460683febe516ab4
Yunusali <37039084+Khakimjanovich@users.noreply.github.com>
2022-05-02 22:48:05 +0500

    E-14: Named Slots in a Nutshell

--- main.js
+++ main.js
@@ -1,40 +1,28 @@
-// window.Event = new Vue();
-window.Event = new class {
-    constructor() {
-        this.vue = new Vue();
-    }
-
-    fire(event, data = null) {
-        this.vue.$emit(event, data)
-    }
-
-    listen(event, callback) {
-        this.vue.$on(event, callback)
-    }
-};
-
-Vue.component('coupon', {
+Vue.component('modal', {
     template: `
-            <input placeholder="Enter your coupon code" @blur="onCouponApplied"/>    
-        `,
-
-    methods: {
-        onCouponApplied() {
-            // Event.$emit('applied')
-            Event.fire('applied')
-        }
-    }
+            <div class="modal is-active">
+                <div class="modal-background"></div>
+                <div class="modal-card">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">
+                            <slot name="header"></slot>
+                        </p>
+                        <button class="delete" aria-label="close"></button>
+                    </header>
+                    <section class="modal-card-body">
+                        <slot>
+                        </slot>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <slot name="footer"></slot>
+                    </footer>
+                </div>
+            </div>
+        `
 })
 
 new Vue({
-    el: '#root', data: {
-        couponApplied: false
-    },
+    el: '#root',
 
-    created() {
-        // Event.$on('applied', () => {
-        Event.listen('applied', () => {
-            this.couponApplied = true;
-        })
-    }
+
 })
