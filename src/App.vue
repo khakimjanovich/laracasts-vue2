f5f96a3a378082de53759afb516caa9b06c694d2
Yunusali <37039084+Khakimjanovich@users.noreply.github.com>
2022-05-03 06:22:48 +0500

    E-16: Webpack and vue-cli

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <message>Hello there</message>
+    <message>HI again</message>
+  </div>
+</template>
+
+<script>
+import message from "./components/Message";
+
+export default {
+  name: 'App',
+  components: {message},
+  data() {
+    return {}
+  }
+}
+</script>
